a9fedf22da8df5902597969880f15d15bfaa892e
Juan Igancio Navarro <juannava0208@gmail.com>
2020-07-05 23:46:48 -0600

    #1 LogIn Added

---
+++ P2Front/P2Front/Constants/User.cs
@@ -0,0 +1,62 @@
+﻿using Pyecto2Datos1Fontend.ConstantModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace P2Front.Constants
+{
+    /**
+     * Defines objects with all the attributes needed in an user (specified in the intructions)
+     * Not every attribute is needed to create an user, we will change the constructor
+     * when implementing the server classes made in java
+     */
+    class User
+    {
+        public String Email { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public String Password { get; set; }
+        public String Username { get; set; }
+        public Image UserPicture { get; set; }
+        public HashSet<Recipe> MyMenu { get; set; } //I'm not sure of this
+        public List<String> Following { get; set; }
+        public List<String> Followers { get; set; }
+        public List<Recipe> SharedRecipes { get; set; }
+
+        public User() { }
+
+        public User(String email, String password)
+        {
+            if (email == null)
+                email = "";
+            else if (password == null)
+                password = "";
+            this.Email = email;
+            this.Password = password;
+        }
+
+        public User(String email, String name, int Age, String password, String username, Image userPicture,
+            HashSet<Recipe> myMenu, List<String> following, List<String> followers, List<Recipe> sharedRecipes)
+        {
+            this.Email = email;
+            this.Name = name;
+            this.Age = Age;
+            this.Password = password;
+            this.Username = username;
+            this.UserPicture = userPicture;
+            this.MyMenu = myMenu;
+            this.Following = following;
+            this.Followers = followers;
+            this.SharedRecipes = sharedRecipes;
+        }
+
+        public bool CheckLogInInformation()
+        {
+            if (!this.Email.Equals("") && !this.Password.Equals(""))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ P2Front/P2Front/Views/LogIn.xaml.cs
@@ -0,0 +1,35 @@
+﻿using P2Front.Constants;
+using Pyecto2Datos1Fontend.ConstantModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pyecto2Datos1Fontend.ViewsModels
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogIn : ContentPage
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(entry_email.Text, entry_password.Text);
+            if (user.CheckLogInInformation())
+            {
+                DisplayAlert("Login", "Login Success", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login not correct; empty username or password", "Ok");
+            }
+        }
+    }
+}
