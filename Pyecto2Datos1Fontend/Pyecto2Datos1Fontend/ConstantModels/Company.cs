b2d5c17067d27dc393bfe916e3ba493aeceabb64
Juan Igancio Navarro <juannava0208@gmail.com>
2020-07-05 19:27:21 -0600

    Constants Added
    The constants objects are the ones that are pushed to the server, they need a specific format

---
+++ Pyecto2Datos1Fontend/Pyecto2Datos1Fontend/ConstantModels/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Pyecto2Datos1Fontend.ConstantModels
+{
+    class Company
+    {
+        public String Name { get; set; }
+        public Image Logo { get; set; }
+        public String ContactMethod { get; set; }
+        public String Schedule { get; set; }
+        public String Location { get; set; } //This has to be changed (not a string)
+
+        public Company() { }
+        public Company(String name, Image logo, String contactMethod, String schedule, String location)
+        {
+            this.Name = name;
+            this.Logo = logo;
+            this.ContactMethod = contactMethod;
+            this.Schedule = schedule;
+            this.Location = location;
+        }
+    }
+}
---
+++ Pyecto2Datos1Fontend/Pyecto2Datos1Fontend/ConstantModels/Recipe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Pyecto2Datos1Fontend.ConstantModels
+{
+    /**
+     * Defines objects with all the attributes needed in a recipe (specified in the intructions)
+     * Not every attribute is needed to create a recipe, we will change the constructor
+     * when implementing the server classes made in java
+     */
+    class Recipe
+    {
+        public String Name { get; set; }
+        public String Author { get; set; }
+        public String Meal { get; set; }
+        public int Portions { get; set; }
+        public String PreparingTime { get; set; }
+        public String Difficulty { get; set; }
+        public String DietTags { get; set; }
+        public Image RecipeImage { get; set; }
+        public String Instructions { get; set; }
+        public float Price { get; set; }
+
+        public Recipe() { }
+
+        public Recipe(String name, String author, String meal, int portions, String preparingTime,
+             String difficulty, String dietTags, Image recipeImage, String instructions, float price)
+        {
+            this.Name = name;
+            this.Author = author;
+            this.Meal = meal;
+            this.Portions = portions;
+            this.PreparingTime = preparingTime;
+            this.Difficulty = difficulty;
+            this.DietTags = dietTags;
+            this.RecipeImage = recipeImage;
+            this.Instructions = instructions;
+            this.Price = price;
+        }
+        
+
+    }
+}
