7baec8e8a1a9e91e9976b33710cfdfcd07d54797
Andrés Martínez Vargas <49073466+vmartinezandres@users.noreply.github.com>
2020-07-25 01:45:14 -0600

    The chef query is implemented

---
+++ CookTime/CookTime/Models/ChefQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CookTime.Models
+{
+    public class ChefQuery
+    {
+        
+        [JsonProperty(PropertyName = "user")]
+        public string User { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+        
+    }
+}
---
+++ CookTime/CookTime/Views/ChefQueryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CookTime.Views
+{
+    public partial class ChefQueryPage : ContentPage
+    {
+        public ChefQueryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
